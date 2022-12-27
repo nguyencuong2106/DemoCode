bdc3b5426f62f53de5742918594efaeabc30a31a
nguyen cuong <cuongdola2106@gmail.com>
2022-12-27 07:47:21 +0700

    update code

--- Demo/src/demo/Demo.java
+++ Demo/src/demo/Demo.java
@@ -14,7 +14,8 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!");
+        System.out.println("hello world 2");
     }
     
 }

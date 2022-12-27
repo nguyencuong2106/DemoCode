2e70d044c934dd2b23a8dd79fe525f9027e7b4f9
nguyen cuong <cuongdola2106@gmail.com>
2022-12-27 18:58:20 +0700

    update code  or feature-test

--- Demo/src/demo/Demo.java
+++ Demo/src/demo/Demo.java
@@ -16,6 +16,9 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello world 2");
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b);
     }
     
 }
